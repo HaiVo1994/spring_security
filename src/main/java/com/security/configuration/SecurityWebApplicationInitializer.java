c3338fac4f27e62142e200ff04ad522b24647260
haivo1994 <vophihai1994@gmail.com>
2019-12-17 11:30:13 +0700

    Fix thymeleaf-extras-springsecurity5

--- src/main/java/com/security/configuration/SecurityWebApplicationInitializer.java
+++ src/main/java/com/security/configuration/SecurityWebApplicationInitializer.java
@@ -1,4 +1,7 @@
 package com.security.configuration;
 
-public class SecurityWebApplicationInitializer {
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer
+        extends AbstractSecurityWebApplicationInitializer {
 }
